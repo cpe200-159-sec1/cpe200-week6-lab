a28cbb482ac5c78348f11cd0d6237b414fcae134
ather <kandanich@hotmail.com>
2016-09-27 16:45:12 +0700

    Fin 6-1

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,38 +1,58 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public IOperand firstOperand;
-    public IOperand secondOperand;
+    private IOperand firstOperand;
+    private IOperand secondOperand;
+
+    private BigDecimal x;
+    private BigDecimal y;
+
 
     public BinaryCalculator() {
+        x = new BigDecimal(0);
+        y = new BigDecimal(0);
     }
 
     public void setFirstOperand(IOperand operand) {
+        x = new BigDecimal(operand.getOperand());
     }
 
     public void setSecondOperand(IOperand operand) {
+        y = new BigDecimal(operand.getOperand());
     }
 
     public String add() throws RuntimeException {
-        return null;
+        if(x.intValue() < 0 || y.intValue() < 0){
+            throw new RuntimeException("Not negative");
+        }return x.add(y).stripTrailingZeros().toString();
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        if(x.intValue() < 0 || y.intValue() < 0){
+            throw new RuntimeException("Not negative");
+        }return x.subtract(y).stripTrailingZeros().toString();
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        if(x.intValue() < 0 || y.intValue() < 0){
+            throw new RuntimeException("Not negative");
+        }return x.multiply(y).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        if(x.intValue() < 0 || y.intValue() < 0){
+            throw new RuntimeException("Not negative");
+        }return x.divide(y, 5, BigDecimal.ROUND_UP).stripTrailingZeros().toString();
     }
 
     public String power() throws RuntimeException {
-        return null;
+        if(x.intValue() < 0 || y.intValue() < 0){
+            throw new RuntimeException("Not negative");
+        }return x.pow(y.intValue()).stripTrailingZeros().toString();
     }
 
 }
