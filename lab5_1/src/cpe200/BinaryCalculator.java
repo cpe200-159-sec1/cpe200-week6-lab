2e2e3b447a4c1ea8ec4be9dfa1a40b9925459273
apboomap277 <tanin.wan7275@gmail.com>
2016-09-27 16:40:21 +0700

    580610646

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,38 +1,60 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public IOperand firstOperand;
-    public IOperand secondOperand;
+    private IOperand firstOperand;
+    private IOperand secondOperand;
 
     public BinaryCalculator() {
     }
 
-    public void setFirstOperand(IOperand operand) {
-    }
+    public void setFirstOperand(IOperand operand) { firstOperand = operand; }
 
     public void setSecondOperand(IOperand operand) {
+        secondOperand = operand;
     }
 
     public String add() throws RuntimeException {
-        return null;
+        BigDecimal first = new BigDecimal(firstOperand.getOperand());
+        BigDecimal second = new BigDecimal(secondOperand.getOperand());
+        if(first.compareTo(BigDecimal.ZERO) <= 0 || second.compareTo(BigDecimal.ZERO) <= 0)
+            throw  new RuntimeException("non-negative");
+        else return first.add(second).stripTrailingZeros().toString();
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        BigDecimal first = new BigDecimal(firstOperand.getOperand());
+        BigDecimal second = new BigDecimal(secondOperand.getOperand());
+        if(first.compareTo(BigDecimal.ZERO) <= 0 || second.compareTo(BigDecimal.ZERO) <= 0)
+            throw  new RuntimeException("non-negative");
+        else return first.subtract(second).stripTrailingZeros().toString();
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        BigDecimal first = new BigDecimal(firstOperand.getOperand());
+        BigDecimal second = new BigDecimal(secondOperand.getOperand());
+        if(first.compareTo(BigDecimal.ZERO) <= 0 || second.compareTo(BigDecimal.ZERO) <= 0)
+            throw  new RuntimeException("non-negative");
+        else return first.multiply(second).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        BigDecimal first = new BigDecimal(firstOperand.getOperand());
+        BigDecimal second = new BigDecimal(secondOperand.getOperand());
+        if(first.compareTo(BigDecimal.ZERO) <= 0 || second.compareTo(BigDecimal.ZERO) <= 0)
+            throw  new ArithmeticException("ERROR");
+        else return first.divide(second,5,BigDecimal.ROUND_HALF_UP).stripTrailingZeros().toString();
     }
 
     public String power() throws RuntimeException {
-        return null;
+        BigDecimal first = new BigDecimal(firstOperand.getOperand());
+        BigDecimal second = new BigDecimal(secondOperand.getOperand());
+        if(first.compareTo(BigDecimal.ZERO) <= 0 || second.compareTo(BigDecimal.ZERO) <= 0)
+            throw  new RuntimeException("non-negative");
+        else return first.pow(second.intValue()).stripTrailingZeros().toString();
     }
 
 }
