abf2db349ff64182f7a7f3d1709dc77c30ec64eb
Wiphob <wiphobkaoian@gmail.com>
2016-09-27 15:23:10 +0700

    Finished Lab 6-2 Users

--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -8,43 +8,67 @@ import java.util.ArrayList;
  */
 public class Users {
     public ArrayList<IUser> userList;
+    public Users(){
+        userList = new ArrayList<>();
+    }
 
     public void addUser(IUser user)
     {
+        userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        User NEWuser = new User();
+        NEWuser.setUserName(userName);
+        NEWuser.setPassword(password);
+        userList.add(NEWuser);
     }
 
     public void deleteUser(IUser user)
     {
-
+        if(!(userList.remove(user))) throw  new RuntimeException("user does not exists.");
     }
 
     public boolean exists(IUser user)
     {
-        return false;
+
+        return userList.contains(user);
     }
 
     public boolean usernameExists(String username)
     {
+
+        for (IUser user: userList){
+            if (user.getUserName().equals(username)){
+                return true;
+            }
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public IUser getUserByUsername(String userName)
     {
+
+
+        for (IUser user: userList){
+            if (user.getUserName().equals(userName)){
+                return user;
+            }
+        }
         return null;
     }
 
     public int count()
     {
-        return 0;
+
+        return userList.size();
     }
 
     public IUser[] getUserArray()
     {
-        return null;
+
+        return userList.toArray(new User[count()]);
     }
 }
