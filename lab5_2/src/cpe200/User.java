4ca66a31f1fd4e18c9aebf0c2befb3fbbdbbdeba
Uttakran Renuman <qowieury_kidmaioak@hotmail.com>
2016-09-27 15:54:01 +0700

    All complete

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,4 +4,83 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User implements IUser {
+    public String userName;
+    public String password;
+
+    public User(){
+
+    }
+    public User(String user ,String pass) {
+        setUserName(user);
+        setPassword(pass);
+    }
+
+    public String setUserName(String name) {
+        if(name != null) {
+            String tmp = userName;
+            if (isUsernameValid(name)) {
+                userName = name;
+            } else {
+                throw new RuntimeException();
+            }
+            return tmp;
+        }return null;
+    }
+
+    public int setPassword(String name) {
+        if (name != null) {
+            if (isPasswordValid(name)) {
+                password = name;
+            } else {
+                throw new RuntimeException();
+            }
+            return password.length();
+        }return 0;
+    }
+
+    public String getUserName() {
+
+        return userName;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+
+    public boolean isUsernameValid(String name) {
+        if (name.length() < 8){//check lengh >=8
+            return false;
+        }else if (Character.isDigit(name.charAt(0)) == true) {
+            return false;
+        }else {
+            for (int i = 0; i < name.length(); i++) {
+                if (Character.isAlphabetic(name.charAt(i)) == false) {
+                    if (Character.isDigit(name.charAt(i)) == false){
+                        return false;
+                    }
+                }
+            }
+            return true;
+
+
+        }
+    }
+    public boolean isPasswordValid(String name){
+        if (name.length() >=12){
+            boolean haveDigit=false,haveCapital=false,haveSmall=false;
+            for (int i=0;i<name.length();i++)
+            {
+                if(Character.isUpperCase(name.charAt(i))) haveCapital=true;
+                else if(Character.isLowerCase(name.charAt(i))) haveSmall=true;
+                else if(Character.isDigit(name.charAt(i))) haveDigit=true;
+                if(haveCapital && haveDigit && haveSmall){
+                    password = name;
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    }
 }
--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -9,42 +9,66 @@ import java.util.ArrayList;
 public class Users {
     public ArrayList<IUser> userList;
 
+    public Users(){
+        userList = new ArrayList<IUser>();
+    }
+
     public void addUser(IUser user)
     {
+        userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        IUser user = new User(userName,password);
+        userList.add(user);
     }
 
     public void deleteUser(IUser user)
     {
+        if(userList.size() == 0){
+            throw new RuntimeException();
+        }
+        userList.remove(userList.indexOf(user));
 
     }
 
     public boolean exists(IUser user)
     {
-        return false;
+        return userList.contains(user);
     }
 
     public boolean usernameExists(String username)
     {
+        for(int i=0;i<userList.size();i++){
+            if (username.equals(userList.get(i).getUserName())){
+                return true;
+            }
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public IUser getUserByUsername(String userName)
     {
-        return null;
+        for(int i=0;i<userList.size();i++){
+            if(userList.get(i).getUserName().equals(userName)) {
+                return userList.get(i);
+            }
+        } return new User();
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public IUser[] getUserArray()
     {
-        return null;
+        IUser[] userArray = new User[userList.size()];
+        for (int i =0;i<userList.size();i++){
+            userArray[i] = userList.get(i);
+        }
+        return userArray;
     }
 }
