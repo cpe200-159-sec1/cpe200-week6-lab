350c9ed9112991a1f0d46c6ece145cfcc0d16fcb
smile1218 <supitcha.sawasd@hotmail.com>
2016-09-27 17:14:42 +0700

    580610684

--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -7,44 +7,69 @@ import java.util.ArrayList;
  * Created by pruet on 12/9/2559.
  */
 public class Users {
+
+
     public ArrayList<IUser> userList;
+
+    public Users(){
+        userList = new ArrayList<>();
+    };
 
     public void addUser(IUser user)
     {
+        userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        User user = new User();
+        user.setUserName(userName);
+        user.setPassword(password);
+        userList.add(user);
     }
 
     public void deleteUser(IUser user)
     {
-
+        if (userList.size() > 0 ) {
+            userList.remove(user);
+        }else {
+            throw new RuntimeException("Empty!");
+        }
     }
 
     public boolean exists(IUser user)
     {
-        return false;
+        return userList.contains(user);
     }
 
     public boolean usernameExists(String username)
     {
+        for (int i = 0; i < userList.size(); i++) {
+            if (username == userList.get(i).getUserName()) {
+                return true;
+            }
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public IUser getUserByUsername(String userName)
     {
+        for (int i = 0; i < userList.size(); i++) {
+            if (userName.equals(userList.get(i).getUserName())) {
+                return userList.get(i);
+            }
+        }
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public IUser[] getUserArray()
     {
-        return null;
+        return  userList.toArray(new User[1]);
     }
 }
