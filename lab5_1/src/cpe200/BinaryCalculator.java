48f571bc417daa616508b949efeb558c50519027
kohSatrarin <koh.satrarin@gmail.com>
2016-09-27 15:22:55 +0700

    580610679

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,38 +1,72 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BinaryCalculator {
-    public IOperand firstOperand;
-    public IOperand secondOperand;
+    private BigDecimal firstOperand;
+    private BigDecimal secondOperand;
 
     public BinaryCalculator() {
+        firstOperand = BigDecimal.ZERO;
+        secondOperand = BigDecimal.ZERO;
     }
 
     public void setFirstOperand(IOperand operand) {
+        firstOperand = new BigDecimal(operand.getOperand());
     }
 
     public void setSecondOperand(IOperand operand) {
+        secondOperand = new BigDecimal(operand.getOperand());
     }
 
-    public String add() throws RuntimeException {
-        return null;
+    public String add(){
+        checkNegativeVaule();                                //check that input >= 0
+        return firstOperand.add(secondOperand).stripTrailingZeros().toString();
+                                                            //toString bc rq String
     }
 
-    public String subtract() throws RuntimeException {
-        return null;
+    public String subtract(){
+        checkNegativeVaule();                               //check that input >= 0
+        return firstOperand.subtract(secondOperand).stripTrailingZeros().toString();
+                                                            //toString bc rq String
     }
 
-    public String multiply() throws RuntimeException {
-        return null;
+    public String multiply(){
+        checkNegativeVaule();                               //check that input >= 0
+        return firstOperand.multiply(secondOperand).stripTrailingZeros().toString();
+                                                            //toString bc rq String
     }
 
     /* This method should throw an exception when divide by zero */
-    public String division() throws RuntimeException {
-        return null;
+    public String division(){
+        checkNegativeVaule();
+        checkDivideByZero();
+        return firstOperand.divide(secondOperand,5, RoundingMode.HALF_UP).stripTrailingZeros().toString();
     }
 
     public String power() throws RuntimeException {
-        return null;
+        checkNegativeVaule();
+        return BigDecimal.valueOf(
+                Math.pow(
+                        firstOperand.doubleValue(),
+                        secondOperand.doubleValue()
+                                                    )
+                                                        ).stripTrailingZeros().toString();
     }
+
+    private void checkNegativeVaule() {
+        if (firstOperand.compareTo(BigDecimal.ZERO) < 0 || secondOperand.compareTo(BigDecimal.ZERO) < 0) {
+            throw new RuntimeException("Error, input less then 0");
+            }
+        }
+
+    private void checkDivideByZero() {
+        if (secondOperand.compareTo(BigDecimal.ZERO) == 0) {
+            throw new ArithmeticException();
+        }
+    }
+
 
 }
--- lab5_2/src/cpe200/Users.java
+++ lab5_2/src/cpe200/Users.java
@@ -7,44 +7,70 @@ import java.util.ArrayList;
  * Created by pruet on 12/9/2559.
  */
 public class Users {
-    public ArrayList<IUser> userList;
+
+    private ArrayList<IUser> userList = new ArrayList<>();
 
     public void addUser(IUser user)
     {
+        userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        //incase of input is string, so we have to conv it to object first
+        User newUser = new User();
+        newUser.setUserName(userName);
+        newUser.setPassword(password);
+        userList.add(newUser);
+
     }
 
     public void deleteUser(IUser user)
     {
-
+        //userList.remove(user);
+        if(!userList.remove(user)) {
+            throw new RuntimeException();
+        }
     }
 
     public boolean exists(IUser user)
     {
+
+        if (userList.contains(user)){
+            return true;
+        }
+
         return false;
     }
 
     public boolean usernameExists(String username)
     {
+        for (IUser user : userList) {
+            if (user.getUserName().equals(username))
+                return true;
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public IUser getUserByUsername(String userName)
     {
+        for (IUser user : userList) {
+            if (user.getUserName().equals(userName))
+                return user;
+        }
+
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public IUser[] getUserArray()
     {
-        return null;
+
+        return userList.toArray(new User[1]);
     }
 }
