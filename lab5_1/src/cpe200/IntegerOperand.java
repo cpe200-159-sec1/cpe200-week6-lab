a1d1a650aa7c68ac51547aaef91249b8eae00ac2
sharof <pro21808@gmail.com>
2016-10-12 07:59:20 +0700

    580610659

--- lab5_1/src/cpe200/IntegerOperand.java
+++ lab5_1/src/cpe200/IntegerOperand.java
@@ -1,16 +1,14 @@
 package cpe200;
-
-/**
- * Created by pruet on 26/9/2559.
- */
 public class IntegerOperand implements IOperand {
-
+    private int operand;
     public IntegerOperand(int i) {
-
+        this.setOperand(i);
     }
-
     @Override
     public String getOperand() {
-        return null;
+        return Integer.toString(operand);
+    }
+    public void setOperand(int operand) {
+        this.operand = operand;
     }
 }
