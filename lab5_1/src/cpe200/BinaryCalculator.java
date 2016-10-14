1948f36c9e527bc2f877803dd5239a583e853df4
magmanex <timecrisis1995@live.com>
2016-10-14 21:04:23 +0700

    580610692

--- lab5_1/src/cpe200/BinaryCalculator.java
+++ lab5_1/src/cpe200/BinaryCalculator.java
@@ -1,38 +1,80 @@
 package cpe200;
 
 
-public class BinaryCalculator {
-    public IOperand firstOperand;
-    public IOperand secondOperand;
+import java.math.BigDecimal;
 
-    public BinaryCalculator() {
+public class BinaryCalculator {
+    private IOperand firstOperand;
+    private IOperand secondOperand;
+
+    public BinaryCalculator()
+    {
     }
 
     public void setFirstOperand(IOperand operand) {
+        firstOperand = operand;
     }
 
     public void setSecondOperand(IOperand operand) {
+        secondOperand = operand;
     }
 
     public String add() throws RuntimeException {
-        return null;
+        BigDecimal first = new BigDecimal(firstOperand.getOperand());
+        BigDecimal second = new BigDecimal(secondOperand.getOperand());
+        if(first.intValue()<0 || second.intValue()<0)
+        {
+            throw new RuntimeException("number is lower than zero");
+        }
+        BigDecimal sum = first.add(second);
+        return sum.stripTrailingZeros().toString();
     }
 
     public String subtract() throws RuntimeException {
-        return null;
+        BigDecimal first = new BigDecimal(firstOperand.getOperand());
+        BigDecimal second = new BigDecimal(secondOperand.getOperand());
+        if(first.intValue()<0 || second.intValue()<0)
+        {
+            throw new RuntimeException("number is lower than zero");
+        }
+        BigDecimal sum = first.subtract(second);
+        return sum.stripTrailingZeros().toString();
     }
 
     public String multiply() throws RuntimeException {
-        return null;
+        BigDecimal first = new BigDecimal(firstOperand.getOperand());
+        BigDecimal second = new BigDecimal(secondOperand.getOperand());
+        if(first.intValue()<0 || second.intValue()<0)
+        {
+            throw new RuntimeException("number is lower than zero");
+        }
+        BigDecimal sum = first.multiply(second);
+        return sum.stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws RuntimeException {
-        return null;
+        BigDecimal first = new BigDecimal(firstOperand.getOperand());
+        BigDecimal second = new BigDecimal(secondOperand.getOperand());
+        if(first.intValue()<0 || second.intValue()<0)
+        {
+            throw new RuntimeException("number is lower than zero");
+        }
+        BigDecimal out = first.divide(second,5,4);
+
+        return out.stripTrailingZeros().toString();
     }
 
     public String power() throws RuntimeException {
-        return null;
+        BigDecimal first = new BigDecimal(firstOperand.getOperand());
+        BigDecimal second = new BigDecimal(secondOperand.getOperand());
+        if(first.intValue()<0 || second.intValue()<0)
+        {
+            throw new RuntimeException("number is lower than zero");
+        }
+        BigDecimal out = first.pow(second.intValue());
+
+        return out.stripTrailingZeros().toString();
     }
 
 }
--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,4 +4,47 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User implements IUser {
+    private String userName;
+    private String password;
+    private String userPattern = "^[A-Za-z][A-Za-z0-9]{7,}$";
+    private String passPattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{12,}$";
+
+    public User() {
+        userName = null;
+        password = null;
+    }
+
+    public User(String name, String pass) {
+        userName = null;
+        password = null;
+        setUserName(name);
+        setPassword(pass);
+    }
+
+    public String setUserName(String name) {
+        if (name == null)
+            return null;
+        else if (name.equals("") || !name.matches(userPattern))
+            throw new RuntimeException("Invalid username");
+        String oldUserName = userName;
+        userName = name;
+        return oldUserName;
+    }
+
+    public int setPassword(String name) {
+        if (name == null)
+            return password.length();
+        else if (name.equals("") || !name.matches(passPattern))
+            throw new RuntimeException("Invalid password");
+        password = name;
+        return password.length();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
